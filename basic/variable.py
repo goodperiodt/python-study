0d704ce21d41f0d75b23a2ff7ee27f099119ed08
goodperiodt <ilypsj@gmail.com>
2024-05-28 14:30:23 +0900

    #1. 파이썬 기초문법

---
+++ basic/variable.py
@@ -0,0 +1,30 @@
+# 파이썬 파일의 확장자: (*.py)
+# Ctrl + Alt + N -> 실행 단축키
+
+'''
+* 변수 (variable)
+
+1. 변수는 데이터를 저장하기 위한 공간에 이름을 붙인 것
+2. 하나의 변수에는 하나의 데이터만 저장 가능.
+3. 언제든지 변경이 가능하다.
+'''
+
+# 파이썬은 타입을 작성하지 않는다.
+# 타입이 없는 게 아니라 동적 타이핑 언어다.
+num = 10 + 9
+print(type(num))
+
+num = 50 + 40 # 변수의 값 변경도 일반 선언과 동일하다.
+print(num)
+
+# 선언되지 않은 변수는 사용이 불가능하다.
+# print(result)
+
+# 파이썬 변수 타입
+# Number: int, float, boolean, complex(복소수형)
+# Sequence(순차형): string, list, tuple
+# Map: dictionary(key와 value 형태로 저장된다.)
+# Set: set
+# None: 그 어떠한 값도 참조하지 않는다.
+
+
